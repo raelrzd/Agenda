c8d32fb27ee6a2dad6eeea99c7f16a30e9eefb3a
israel <israelrezendeluiz2903@gmail.com>
2022-08-23 11:24:03 -0300

    incluido aluno harold e adicionado mensagem ao abrir app

--- app/src/main/java/rezende/israel/alura/MainActivity.java
+++ app/src/main/java/rezende/israel/alura/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -17,12 +17,12 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // BALÃO INFERIOR AO ABRIR O APP
-//        Toast.makeText(this, "Rael Rezende", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Seja bem vindo!! App em construção...", Toast.LENGTH_LONG).show();
         // APRESENTA ARQUIVO DE ACTIVITY
         setContentView(R.layout.activity_main);
 
         List<String> alunos = new ArrayList<>(
-                Arrays.asList("Raelzera", "Guizera", "Teteuzera"));
+                Arrays.asList("Raelzera", "Guizera", "Teteuzera", "Harold"));
         ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,alunos));
 
