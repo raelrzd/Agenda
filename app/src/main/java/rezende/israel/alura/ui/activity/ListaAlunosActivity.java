6190810fd87bc19984a8c0384d8dc3d6efebd3ff
israel <israelrezendeluiz2903@gmail.com>
2022-08-26 15:20:51 -0300

    realizado testes na lista de alunos em relação ao Scrool da tela, implementado no formulario de cadastro de aluno ScrollView para visualização completa dos itens caso necessário.

--- app/src/main/java/rezende/israel/alura/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/rezende/israel/alura/ui/activity/ListaAlunosActivity.java
@@ -39,9 +39,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setTitle(TITULO_APPBAR);
         configuraFabNovoAluno();
         configuraLista();
-        dao.salva(new Aluno("Rael", "14997225375", "rael@gmail.com"));
-        dao.salva(new Aluno("Gui", "1111111111", "gui@gmail.com"));
-
+        for (int i = 0; i < 10; i++) {
+            dao.salva(new Aluno("Rael", "14997225375", "rael@gmail.com"));
+            dao.salva(new Aluno("Gui", "1111111111", "gui@gmail.com"));
+        }
 
     }
 
